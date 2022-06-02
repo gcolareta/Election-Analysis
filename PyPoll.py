6615abdd1a2c8985432bda882bb06ecc6a78c212
awakekibbles <gcolareta@gmail.com>
2022-06-02 06:31:45 -0400

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -1,34 +1,93 @@
 # the data we need to retrieve:
-
-# assign a variable for the file to load
-# file_to_load = 'Resources/election_results.csv'
-# with open(file_to_load) as election_data:
-    #print(election_data)
+# 1. the total number of votes cast
+# 2. a complete list of candidates who received votes
+# 3. the total number of votes each candidate won
+# 4. the percentage of votes each candidate won
+# 5. the winner of the election based on popular votes
 
 # Add dependencies, use indirect method
 import csv
 import os
-
 # Assign a variable to load a file from a path
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize total_votes counter
+total_votes = 0
+
+# declare candidate options list
+candidate_options = []
+# declare dictionary for candidate votes
+candidate_votes = {}
+
+# winning candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 # open the election_results file and read the file
 with open(file_to_load) as election_data:
-    #to do: read and analyze the data here
     # Read the file object with the reader function
     file_reader = csv.reader(election_data)
-    # print each row
-    #for row in file_reader:
-        #print(row)
 
-    # Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+    
+    # print each row
+    for row in file_reader:
+        # Add to the total_votes count
+        total_votes += 1
+        # print total_votes
+        #print(total_votes)
+        
+        # print the candidate name from each row
+        candidate_name = row[2]
+  
+        # if the candidate doesn't match any existing candidate, add it to the list of candidates
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            # begin tracking candidate's vote count
+            candidate_votes[candidate_name] = 0
+        # add votes to that candidate's count. Keep outside if statement so it can loop through rows with for statement
+        candidate_votes[candidate_name] += 1
+
+# print candidate list
+print(candidate_votes)
+
+# determine the percentage of votes per candidate by looping through the counts
+# iterate through the candidate list
+for candidate_name in candidate_votes:
+    # retrieve vote count for a candidate with the variable vote using the key method
+    vote = candidate_votes[candidate_name]
+    # calculate the percentage of votes
+    vote_percentage = float(vote)/float(total_votes) * 100
+    # print candidate name, vote count and % of votes
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({vote:,})\n")
+
+    # determine winning vote count and candidate
+    # determine if the votes is gretater than the winning count
+    if (vote > winning_count) and (vote_percentage > winning_percentage):
+        # if true then set winning_count = vote and winning_percent = vote_percentage
+        winning_count = vote
+        winning_percentage = vote_percentage
+        # and set the winning candidate = candidate_name
+        winning_candidate = candidate_name
+# TO DO: print the winning candidate, vote count and percentage to terminal
+winning_candidate_summary = (
+    f"----------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winnning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"----------------------------\n")
+print(winning_candidate_summary)
+
+
+
 
 # open the file to save results
-##open(file_to_save, "w")
+# open(file_to_save, "w")
 # use the open statement to open the file as a text file
 #with open(file_to_save, "w") as txt_file:
     # write data to the file
@@ -46,8 +105,3 @@ with open(file_to_load) as election_data:
 
 
 
-# 1. the total number of votes cast
-# 2. a complete list of candidates who received votes
-# 3. the percentage of votes each candidate won
-# 4. the total number of votes each candidate won
-# 5. the winer of the election based on popular votes
--- Python_Practice.py
+++ Python_Practice.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+
+
 print('Hello World')
 counties = list()
 counties = ["Arapahoe", "Denver", "Jefferson"]
@@ -7,5 +10,136 @@ print(counties[2])
 print(counties[0:2])
 counties.append("El Paso")
 print(counties)
+counties.insert(2, "El Paso")
+print(counties)
+counties.remove("El Paso")
+counties.pop(3)
+print(counties)
+counties.insert(1, "El Paso")
+counties.pop(0)
+print(counties)
+counties.remove("Denver")
+counties.append("Denver")
+print(counties)
+counties.append("Arapahoe")
+print(counties)
+counties_dict = {}
+counties_dict['Arapahoe'] = 422829
+print(counties_dict)
+counties_dict['Denver'] = 463353
+counties_dict['Jefferson'] = 432438
+print(counties_dict)
+len(counties_dict)
+print(len(counties_dict))
+counties_dict.items()
+print(counties_dict.items())
+counties_dict.keys()
+print(counties_dict.keys())
+counties_dict.values()
+print(counties_dict.values())
+voting_data = []
+voting_data.append({"county": "Arapahoe", "registered_voters": 422829})
+voting_data.append({"county": "Denver", "registered_voters": 463353})
+voting_data.append({"county": "Jefferson", "registered_voters": 432438})
+print(voting_data)
+voting_data.insert(1, {"county":"El Paso", "registered_voters":461149})
+print(voting_data)
+voting_data.pop(0)
+print(voting_data)
+voting_data.pop(1)
+voting_data.insert(2, {"county":"Denver", "registered_voters":463353})
+print(voting_data)
+voting_data.append({"county":"Arapahoe", "registered_voters":422829})
+print(voting_data)
+
+counties = ["Arapahoe", "Denver", "Jefferson"]
+if counties[1] == "Denver":
+    print(counties[1])
+
+if "El Paso" in counties:
+    print("El Paso is in the list of counties")
+else:
+    print("El Paso is not in the list of counties")
+
+if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties")
+else:
+    print("Arapahoe or El Paso are not in the list of counties")
+
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso are in the list of counties")
+else:
+    print("Arapahoe and El Paso are not in the list of counties")
+
+for county in counties:
+    print(county)
+
+for county in range(3):
+    print(county)
+
+for i in range(len(counties)):
+    print(counties[i])
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+for county in counties_dict.keys():
+    print(county)
+
+for county in counties_dict:
+    print(counties_dict[county])
+
+for county, voters in counties_dict.items():
+    print(county, voters)
+
+for k, v in counties_dict.items():
+    print(k, "county has", v, "registered voters")
+
+voting_data = []
+voting_data.append({"county": "Arapahoe", "registered_voters": 422829})
+voting_data.append({"county": "Denver", "registered_voters": 463353})
+voting_data.append({"county": "Jefferson", "registered_voters": 432438})
+print(voting_data)
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for i in range(len(voting_data)):
+    print(voting_data[i])
+
+for i in range(len(voting_data)):
+    print(voting_data[i]["county"])
+
+for i in range(len(voting_data)):
+    print(voting_data[i])
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    print(county_dict["registered_voters"])
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters:,} registered voters")
 
 
+print(voting_data)
+for county_dict in voting_data:
+    print(
+        f"{county_dict['county']} county has {county_dict['registered_voters']:,} registered voters")
+
+# if-else statement
+temperature = int(input("What is the Temperature Outside?"))
+if temperature > 80:
+    print("Turn on the AC")
+else:
+    print("Open the window")
+
+# while loop
+x = 0
+while x <= 4:
+    print(x)
+    x = x + 1
+
+#count = 7
+# while count < 1:
+    # print("Hello World")
